9ffb79b6d109f48b126f157cc987b63c8d6e7fdd
Alexandra Esteves <leopoldoalexandra@gmail.com>
2024-11-14 09:47:45 -0300

    alterações na main

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import copoAmarelo from "../../assets/images/copo-amarelo.png"
-import bolaVerde from "../../assets/images/eclipse-verde.png"
 import { Principal } from "./style"
 
 
@@ -7,14 +6,14 @@ export default function Home() {
     return (
         <Principal>
             <section className="principalDaHome">
-                <div>
+                <div className="texto-home">
                     <h1> Mais que Café</h1>
-                    <h2>Isso é Starbucks</h2>
+                    <h2>Isso é <span>Starbucks</span></h2>
                     <p>A Starbucks oferece uma variedade de cafés de alta qualidade. Alguns dos cafés mais populares incluem o Caffè Americano, o Cappuccino, o Latte Macchiato e o Espresso. Além disso, a Starbucks oferece bebidas quentes e frias, doces diferenciados e sanduíches.</p>
                     <button>Saiba Mais</button>
                 </div>
-                <div>
-                    <img src={bolaVerde} alt="" />
+                <div className="copos-cores">
+                    
                     <img src={copoAmarelo} alt="" />
                 </div>
             </section>
--- src/Components/Home/style.jsx
+++ src/Components/Home/style.jsx
@@ -2,10 +2,57 @@ import styled from "styled-components";
 
 export const Principal = styled.main`
     display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    
 
 
 
 & .principalDaHome{
     display: flex;
+    width: 50%;
+    min-width: 90%;
+}
+
+& .texto-home {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    & h1 {
+        font-family: "Inter", serif;
+        font-weight: 400;
+    }
+    & h2 {
+        font-weight: 400;
+    }
+    & span {
+        color: #037143;
+        font-weight: 800;
+    }
+    
+
+}
+
+
+& .copos-cores {
+    display: flex;
+    background-color: #037143;
+    border-radius: 50%;
+    position: fixed;
+    top: 50%;
+    right: -20%;
+    width: 800px;
+    height: 800px;
+
+    & img {
+        position: absolute;
+        bottom: 60%;
+        right: 40%;
+        width: 50%;
+    }
 }
 `
+// https://meet.google.com/zvh-ymnu-htt
